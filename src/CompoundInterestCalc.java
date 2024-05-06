173ec7f47d2d405df096cb7b24fb8d8fdefdc18b
grs-se <george_ricesmith@hotmail.com>
2024-05-06 10:29:17 +0100

    Customizing numbers formatters

--- src/CompoundInterestCalc.java
+++ src/CompoundInterestCalc.java
@@ -1,4 +1,5 @@
 import java.math.BigDecimal;
+import java.text.DecimalFormat;
 import java.text.NumberFormat;
 import java.text.ParseException;
 import java.util.Locale;
@@ -6,6 +7,7 @@ import java.util.Locale;
 public class CompoundInterestCalc {
 
     // final: should not be changed after it has been created. Make final if possible.
+    // NumberFormat returns your locale by default
     private static final NumberFormat moneyFormatter = NumberFormat.getCurrencyInstance();
     private static final NumberFormat percentFormatter = NumberFormat.getPercentInstance();
 
@@ -23,10 +25,17 @@ public class CompoundInterestCalc {
     }
 
     public static void main(String[] args) throws ParseException {
+        // this is far inferior to using the getCurrencyInstance which out of the box has a formatter for every place in the world and you dont even have to know those rules
+//        DecimalFormat df = new DecimalFormat("£#,###.##");
+//        DecimalFormat df = new DecimalFormat("£#,###.##;£(#)"); // right of semi-colon = negative
+//        DecimalFormat df = new DecimalFormat("£#,###.00;£(#)"); // give us padding of two decimals - always two decimals
+        DecimalFormat df = new DecimalFormat("£00000000,000.00;£(#)"); // £00,000,163,270.02 = padding to left of decimal
+        DecimalFormat pf = new DecimalFormat("#%"); // 8% // multiply that value by 100, i.e. move decimal point over to right by 2 places: .08 = 8
         // In real life probably wouldn't want to be passing in formatted strings like this as they are highly opinionated
         // However a programme with a UI may want to accept a variety of formats, perhaps b using RegExt to parse
         BigDecimal balance = CompoundInterestCalc.calculate("£25,300.00", "8%", 10, "£7,500");
-        System.out.println(moneyFormatter.format(balance)); // £36,075.81
-
+        System.out.println(df.format(balance)); // £163,270.02
+        System.out.println(df.format(balance.negate())); // £(163,270.02)
+        System.out.println(pf.format(.08));
     }
 }
