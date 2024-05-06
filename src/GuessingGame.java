44fe55915f174835d6d0795af0025e0e970ad427
grs-se <george_ricesmith@hotmail.com>
2024-05-06 15:31:25 +0100

    The Switch Statement

--- src/GuessingGame.java
+++ src/GuessingGame.java
@@ -11,38 +11,102 @@ public class GuessingGame {
         int randomNum = new Random().nextInt(5) + 1;
         System.out.printf("Generated number is: %d.%n", randomNum);
 
+
         // if statement evaluates to a boolean
         // if the statement evaluates to false the thread skips the if block and jumps to the next line of execution
-        if (randomNum == 1) {
-            System.out.println("The color is RED");
-        } else if (randomNum == 2){
-            System.out.println("The color is GREEN");
-        } else if (randomNum == 3) {
-            System.out.println("The color is BLUE");
-        } else {
-            System.out.println("The color is POLKA-DOT");
-        }
+//        if (randomNum == 1) {
+//            System.out.println("The color is RED");
+//        } else if (randomNum == 2){
+//            System.out.println("The color is GREEN");
+//        } else if (randomNum == 3) {
+//            System.out.println("The color is BLUE");
+//        } else {
+//            System.out.println("The color is POLKA-DOT");
+//        }
 
         // CONTROL FLOW INEQUALITIES
-        if (randomNum <= 2) { //
-            System.out.println("The color is RED");
-        } else if (randomNum >= 4) {
-            System.out.println("The color is BLUE");
-        } else {
-            System.out.println("The color is GREEN");
-        }
+//        if (randomNum <= 2) { //
+//            System.out.println("The color is RED");
+//        } else if (randomNum >= 4) {
+//            System.out.println("The color is BLUE");
+//        } else {
+//            System.out.println("The color is GREEN");
+//        }
 
         // COMPOUND STATEMENTS
-        if (randomNum <= 2) { //
-            System.out.println("The color is RED");
-        }
-        // randomNum bigger than 4 and is even number
-        else if (randomNum >= 4 && (randomNum % 2 == 0)) {
-//        else if (randomNum >= 4 || (randomNum % 2 == 0)) {
-            System.out.println("The color is BLUE");
+//        if (randomNum <= 2) { //
+//            System.out.println("The color is RED");
+//        }
+//        // randomNum bigger than 4 and is even number
+//        else if (randomNum >= 4 && (randomNum % 2 == 0)) {
+////        else if (randomNum >= 4 || (randomNum % 2 == 0)) {
+//            System.out.println("The color is BLUE");
+//        } else {
+//            System.out.println("The color is GREEN");
+//        }
+
+        // THE SWITCH STATEMENT
+//        if (randomNum == 1) {
+//            System.out.println("The color is RED");
+//        } else if (randomNum == 2){
+//            System.out.println("The color is GREEN");
+//        } else if (randomNum == 3) {
+//            System.out.println("The color is BLUE");
+//        } else if (randomNum == 4) {
+//            System.out.println("The color is PURPLE");
+//        } else {
+//            System.out.println("The color is whatever");
+//        }
+        // functionally equivalent to if/else statement
+//        switch (randomNum) {
+//            case 1:
+//                System.out.println("The color is RED");
+//                // if we don't have this break here the thread will keep moving through all the cases
+//                // tells the thread to the jump out of the switch block onto the next line, and if no next line then the program just ends
+//                // very easy to forget the break
+//                break;
+//            case 2:
+//                System.out.println("The color is GREEN");
+//            break;
+//            case 3:
+//                System.out.println("The color is BLUE");
+//                break;
+//            case 4:
+//                System.out.println("The color is PURPLE");
+//                break;
+//            default:
+//                System.out.println("The color is whatever");
+        // optional default break
+//                break;
+//        }
+
+//        String command = "stop";
+        String command = null;
+
+        // Most people consider switch statements to be more readable, and switch statements run faster than if statements, due to compiler optimizations
+        // however, the break statements can be easy to forget and not elegant, and not easy way to test for null variables (in versions prior to Java 17)
+        // recommendation against nested if else blocks if possible
+        if (command == null) {
+
         } else {
-            System.out.println("The color is GREEN");
-        }
+            switch (command) {
+                case "stop":
+                    System.out.println("Stopping now...");
+                    // don't need break here!?
+                case "go":
+                    System.out.println("Going now...");
+                default:
+                    System.out.println("Command not understood");
+            }
+
+    }
+
+
+
+
+
+
+
     }
 
 
